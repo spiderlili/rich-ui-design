5c86450b031c6150760ce6fb14fea024aa3c45a5
Jing <jtan1993@gmail.com>
2018-10-21 17:25:32 +0100

    ui

---
+++ Assets/Scripts/ArrangeOrderActions.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Collections;
+
+public class ArrangeOrderActions : MonoBehaviour {
+    private RectTransform panelRectTransform;
+    void Start() {
+        panelRectTransform = GetComponent<RectTransform>();
+    }
+    public void MoveDownOne()
+    {
+        int currentSiblingIndex = panelRectTransform.GetSiblingIndex(); panelRectTransform.SetSiblingIndex(currentSiblingIndex - 1);
+    }
+
+    public void MoveUpOne()
+    {
+        int currentSiblingIndex = panelRectTransform.GetSiblingIndex(); panelRectTransform.SetSiblingIndex(currentSiblingIndex + 1);
+    }
+}
